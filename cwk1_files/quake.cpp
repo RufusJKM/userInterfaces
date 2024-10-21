354ca21c770d3e056bcb384445203a1c31b6367c
RufusJKM <146677707+RufusJKM@users.noreply.github.com>
2024-10-21 11:52:07 +0000

    Completed part 1

--- cwk1_files/quake.cpp
+++ cwk1_files/quake.cpp
@@ -9,7 +9,22 @@ using namespace std;
 Quake::Quake(const string& tm, double lat, double lon, double dep, double mag):
   time(tm), latitude(lat), longitude(lon), depth(dep), magnitude(mag)
 {
-  // Add validation code here
+  if (latitude < MIN_LATITUDE || latitude > MAX_LATITUDE) {
+    throw std::invalid_argument("Latitude out of range");
+  }
+
+  if (longitude < MIN_LONGITUDE || longitude > MAX_LONGITUDE) {
+    throw std::invalid_argument("Longitude out of range");
+  }
+
+  if (depth < 0){
+    throw std::invalid_argument("Can't have negative depth");
+  }
+
+  if (magnitude < 0){
+    throw std::invalid_argument("Can't have negative magnitude");
+  }
+
 }
 
 
