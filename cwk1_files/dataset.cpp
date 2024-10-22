6a768edcc48b674410af30179c6e54c31e652477
RufusJKM <146677707+RufusJKM@users.noreply.github.com>
2024-10-22 11:46:42 +0000

    Implementing methods

--- cwk1_files/dataset.cpp
+++ cwk1_files/dataset.cpp
@@ -1,14 +1,31 @@
 // COMP2811 Coursework 1: QuakeDataset class
 #include <stdexcept>
+#include "csv.hpp>"
 #include "dataset.hpp"
 
 using namespace std;
 
-QuakeDataset::loadData(const std::string& filename){
-    
+void QuakeDataset::loadData(const std::string& filename) const{
+    csv::CSVReader reader("test.csv");
+    for (auto& row: reader) {
+        string time = row["time"].get<>();
+        double latitude = row["latitude"].get<double>();
+        double longitude = row["longitude"].get<double>();
+        double depth = row["depth"].get<double>();
+        double magnitude = row["mag"].get<double>();
+        data.push_back(Quake(time, latitude, longitude, depth, magnitude));
+}
 }
 
-QuakeDataset::strongest(){
+Quake QuakeDataset::operator [] (int index) const{
+    if (index < 0 || index >= data.size()){
+        throw std::invalid_argument("Index out of bounds");
+    } else {
+        return data[index];
+    }
+}
+
+Quake QuakeDataset::strongest() const{
     double highest = 0;
     int quakeNum = 0;
     for (int i = 0; i < data.size(); i++){
@@ -17,10 +34,10 @@ QuakeDataset::strongest(){
             quakeNum = i;
         }
     }
-    return data[i];
+    return data[quakeNum];
 }
 
-QuakeDataset::shallowest(){
+Quake QuakeDataset::shallowest() const{
     double lowest = data[0].getDepth();
     int quakeNum = 0;
     for (int i = 1; i < data.size(); i++){
@@ -29,12 +46,12 @@ QuakeDataset::shallowest(){
             quakeNum = i;
         }
     }
-    return data[i];
+    return data[quakeNum];
 }
 
-QuakeDataset::meanDepth(){
-    double size = data.size();
-    double total = 0;
+double QuakeDataset::meanDepth() const{
+    float size = data.size();
+    float total = 0;
     for (int i = 0; i < size; i++){
         total = total + data[i].getDepth();
     }
@@ -42,7 +59,7 @@ QuakeDataset::meanDepth(){
     return total/size;
 }
 
-QuakeDataset::meanMagnitude(){
+double QuakeDataset::meanMagnitude() const {
     double size = data.size();
     double total = 0;
     for (int i = 0; i < size; i++){
--- cwk1_files/dataset.hpp
+++ cwk1_files/dataset.hpp
@@ -8,15 +8,16 @@
 class QuakeDataset
 {
   public:
-    QuakeDataset() = default;
+    QuakeDataset(): data() {}
     QuakeDataset(const std::string& filename) { loadData(filename); } 
     void loadData(const std::string& filename) const;
-    int size() const { return this->size(); }
-    Quake operator [] (int) const;
+    int size() const { std::cout << "Data size: " << data.size() << std::endl;
+    return this->data.size(); }
+    Quake operator [] (int index) const; 
     Quake strongest() const;
     Quake shallowest() const;
-    Quake meanDepth() const;
-    Quake meanMagnitude() const;
+    double meanDepth() const;
+    double meanMagnitude() const;
     // Specify prototypes or inlined methods here
     // (see UML diagram for what is required)
 
